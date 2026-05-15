68c23d6722d7f9cfa32209838b8388520f48c544
borisavila539 <68365374+borisavila539@users.noreply.github.com>
2026-05-15 16:30:18 -0600

    mejora ventas e inventario

--- inventario.py
+++ inventario.py
@@ -1,40 +1,22 @@
 
-#.venv 
+from datetime import datetime
+import pytz
+import xmlrpc.client
+import pandas as pd
+import pyodbc
+import json
+from prefect import flow, task 
+from pathlib import Path
+
+
+
 prod_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};Server=CUBO-INTERMODA;Database=IMClientesIV;UID=iditm;PWD=Int3r-M0d@.Id@;Trusted_Connection=no;"
 url = "https://unikfashiongt.odoo.com"
 db = "rocketgithub-unikfashiongt-odoo-sh-main-25251833"
 username = "rmartinez@intermoda.com.hn"
 password = "Intermod@2026/?"
 
-#Autenticación con Odoo
-from datetime import datetime
-import xmlrpc.client
-import pandas as pd
-import pyodbc
 
-common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
-uid = common.authenticate(db, username, password, {})
-
-#Obtener el producto por su código de barras
-models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
-products = pd.DataFrame(models.execute_kw(db, uid, password, 'stock.move.line', 'search_read',[[['quantity','>',0]]]))
-print(len(products))
-#products
-
-    
-#obtener los clientes
-with pyodbc.connect(prod_connection_string) as conn:
-    cursor = conn.cursor()
-    cursor.execute("EXEC dbo.SP_ObtenerClientes")
-
-    columns = [column[0] for column in cursor.description]
-    rows = cursor.fetchall()
-
-    df = pd.DataFrame.from_records(rows, columns=columns)
-
-    conn.commit()
-Clientes = df[df['CodigoCliente'] == "IMGT-000001134"]
-#print(Clientes)
 
 
 def obtener_CodigoDeBarraInfo(codigo_barra):
@@ -61,86 +43,111 @@ def borrar_Invetario_Tienda(codigo_tienda):
         cursor.execute(query)
         conn.commit()
 
-#obtener la información del producto por su código de barras
-locations = products['location_dest_id'].apply(lambda x: x[0] if isinstance(x, list) and len(x) > 0 else None).unique()
+@flow(name='Inventario UnikFashion', flow_run_name=generate_flow_run_name, retries=0, retry_delay_seconds=60)
+def inventario():
+    common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
+    uid = common.authenticate(db, username, password, {})
 
-for location in locations:
-    id_ubicacion = int(location)
-    ubicacion = pd.DataFrame(models.execute_kw(db, uid, password, 'stock.location', 'search_read',[[['id','=',id_ubicacion]]],{'limit': 1}))        
-    id_Almacen = ubicacion['location_id'][0]       
-    almacen = pd.DataFrame(models.execute_kw(db, uid, password, 'stock.warehouse', 'search_read',[[['code','=',id_Almacen[1]]]],{'limit': 1}))
-    if almacen.empty:
-        print(f"Almacén no encontrado para ubicación: {location}")
-        continue
-    tienda =almacen['name'][0]     
-    borrar_Invetario_Tienda(tienda) #Borrar el inventario de la tienda antes de insertar los nuevos datos
+    #Obtener el producto por su código de barras
+    models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
+    products = pd.DataFrame(models.execute_kw(db, uid, password, 'stock.move.line', 'search_read',[[['quantity','>',0]]]))
+    print(len(products))
+    #products
 
-    json_data_todo = '['
-    #obtener productos por location_dest_id
-    products_location = products[products['location_dest_id'].apply(lambda x: x[0] if isinstance(x, list) and len(x) > 0 else None) == location]
-    print(f"Procesando Tienda: {tienda}: {len(products_location)} productos")
+        
+    #obtener los clientes
+    with pyodbc.connect(prod_connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("EXEC dbo.SP_ObtenerClientes")
+
+        columns = [column[0] for column in cursor.description]
+        rows = cursor.fetchall()
+
+        df = pd.DataFrame.from_records(rows, columns=columns)
+
+        conn.commit()
+    Clientes = df[df['CodigoCliente'] == "IMGT-000001134"]
+    #print(Clientes)
+    #obtener la información del producto por su código de barras
+    locations = products['location_dest_id'].apply(lambda x: x[0] if isinstance(x, list) and len(x) > 0 else None).unique()
+
+    for location in locations:
+        id_ubicacion = int(location)
+        ubicacion = pd.DataFrame(models.execute_kw(db, uid, password, 'stock.location', 'search_read',[[['id','=',id_ubicacion]]],{'limit': 1}))        
+        id_Almacen = ubicacion['location_id'][0]       
+        almacen = pd.DataFrame(models.execute_kw(db, uid, password, 'stock.warehouse', 'search_read',[[['code','=',id_Almacen[1]]]],{'limit': 1}))
+        if almacen.empty:
+            print(f"Almacén no encontrado para ubicación: {location}")
+            continue
+        tienda =almacen['name'][0]     
+        borrar_Invetario_Tienda(tienda) #Borrar el inventario de la tienda antes de insertar los nuevos datos
+
+        json_data_todo = '['
+        #obtener productos por location_dest_id
+        products_location = products[products['location_dest_id'].apply(lambda x: x[0] if isinstance(x, list) and len(x) > 0 else None) == location]
+        print(f"Procesando Tienda: {tienda}: {len(products_location)} productos")
+        
+        for index, row in products_location.iterrows():
+            product = pd.DataFrame(models.execute_kw(db, uid, password, 'product.product', 'search_read',[[['id','=',row['product_id'][0]]]],{'limit': 1})).rename(columns={'barcode': 'CodigoBarra'})
+            codigo_barra = product['CodigoBarra'].iloc[0]
+            if pd.isna(codigo_barra) or codigo_barra in (False, '', None):
+                continue 
+
+            try:
+                results = obtener_CodigoDeBarraInfo(codigo_barra) #pd.DataFrame.from_records(rows, columns=columns)
+            except Exception as e:
+                print(f"Error al obtener información para el código de barra {codigo_barra}: {e}")
+                continue
+            if results.empty:
+                print(f"No se encontró información para el código de barra {codigo_barra}")
+                continue
+
+            create_date = datetime.strptime(product["create_date"].values[0], "%Y-%m-%d %H:%M:%S")
+            insert_sql = """
+            INSERT INTO Inventario (
+                CodigoCliente, Cliente, CodigoTienda, Tienda, FechaCreacion,
+                Año, NoMes, Mes, Dia, CodigoBarra, CodigoArticulo, Descripcion,
+                CodigoColor, Color, Talla, Linea, Sublinea, Categoria, Base,
+                Genero, Clasificacion, LoteOrigen, PedidoVenta, FechaFactura,
+                Costo, Precio, Cantidad, CostoTotal
+            )
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+            """
+
+            enviar_IntermodaUnikFashion(
+                insert_sql,
+                Clientes['CodigoCliente'].values[0],
+                Clientes['Cliente'].values[0],
+                None,
+                tienda,
+                create_date.strftime("%Y-%m-%d"),
+                create_date.year,
+                create_date.month,
+                create_date.strftime("%B"),
+                create_date.day,
+                product['CodigoBarra'].values[0],
+                results['CodigoArticulo'].values[0],
+                results['Descripcion'].values[0],
+                results['CodigoColor'].values[0],
+                results['Color'].values[0],
+                results['Talla'].values[0],
+                results['Linea'].values[0],
+                results['Sublinea'].values[0],
+                results['Categoria'].values[0],
+                results['Base'].values[0],
+                results['Genero'].values[0],
+                results['ClasificacionAX'].values[0],
+                results['LoteOrigen'].values[0],
+                None,
+                None,
+                None,
+                float(product['list_price'].values[0]),
+                int(row['quantity']),
+                None
+            )    
+        print(f'Datos insertados correctamente en {tienda}')
+    print('Proceso completado')
     
-    for index, row in products_location.iterrows():
-        product = pd.DataFrame(models.execute_kw(db, uid, password, 'product.product', 'search_read',[[['id','=',row['product_id'][0]]]],{'limit': 1})).rename(columns={'barcode': 'CodigoBarra'})
-        codigo_barra = product['CodigoBarra'].iloc[0]
-        if pd.isna(codigo_barra) or codigo_barra in (False, '', None):
-            continue 
-
-        try:
-            results = obtener_CodigoDeBarraInfo(codigo_barra) #pd.DataFrame.from_records(rows, columns=columns)
-        except Exception as e:
-            print(f"Error al obtener información para el código de barra {codigo_barra}: {e}")
-            continue
-        if results.empty:
-            print(f"No se encontró información para el código de barra {codigo_barra}")
-            continue
-
-        create_date = datetime.strptime(product["create_date"].values[0], "%Y-%m-%d %H:%M:%S")
-        insert_sql = """
-        INSERT INTO Inventario (
-            CodigoCliente, Cliente, CodigoTienda, Tienda, FechaCreacion,
-            Año, NoMes, Mes, Dia, CodigoBarra, CodigoArticulo, Descripcion,
-            CodigoColor, Color, Talla, Linea, Sublinea, Categoria, Base,
-            Genero, Clasificacion, LoteOrigen, PedidoVenta, FechaFactura,
-            Costo, Precio, Cantidad, CostoTotal
-        )
-        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
-        """
-
-        enviar_IntermodaUnikFashion(
-            insert_sql,
-            Clientes['CodigoCliente'].values[0],
-            Clientes['Cliente'].values[0],
-            None,
-            tienda,
-            create_date.strftime("%Y-%m-%d"),
-            create_date.year,
-            create_date.month,
-            create_date.strftime("%B"),
-            create_date.day,
-            product['CodigoBarra'].values[0],
-            results['CodigoArticulo'].values[0],
-            results['Descripcion'].values[0],
-            results['CodigoColor'].values[0],
-            results['Color'].values[0],
-            results['Talla'].values[0],
-            results['Linea'].values[0],
-            results['Sublinea'].values[0],
-            results['Categoria'].values[0],
-            results['Base'].values[0],
-            results['Genero'].values[0],
-            results['ClasificacionAX'].values[0],
-            results['LoteOrigen'].values[0],
-            None,
-            None,
-            None,
-            float(product['list_price'].values[0]),
-            int(row['quantity']),
-            None
-        )    
-    print(f'Datos insertados correctamente en {tienda}')
-print('Proceso completado')
-   
 
 
 
--- ventas.py
+++ ventas.py
@@ -34,10 +34,133 @@ def obtener_CodigoDeBarraInfo(codigo_barra):
 
 #Enviar datos a la base de datos de Intermoda
 def enviar_IntermodaUnikFashion(json_data):
+    """
+    Recibe json_data como string JSON o como lista de diccionarios.
+    Inserta o actualiza ventas diarias en [IMClientesIV].[dbo].[Ventas].
+    """
+
+    if isinstance(json_data, str):
+        ventas = json.loads(json_data)
+    else:
+        ventas = json_data
+
+    if isinstance(ventas, dict):
+        ventas = [ventas]
+
     with pyodbc.connect(prod_connection_string) as conn:
         cursor = conn.cursor()
-        query = "EXEC dbo.SP_InsertarVentas ?;"
-        cursor.execute(query, (json_data,))
+
+        for venta in ventas:
+            update_query = """
+                UPDATE [IMClientesIV].[dbo].[Ventas]
+                SET
+                    Cantidad =  ?,
+                    Ganancia = ?,
+                    ImporteTotal =  ?,
+                    GananciaTotal = ?,
+                    CostoTotal =  ?,
+                    FechaModificacion = GETDATE()
+                WHERE Fecha = ?
+                  AND Tienda = ?
+                  AND CodigoBarra = ?;
+            """
+
+            cursor.execute(update_query, (
+                venta["Cantidad"],
+                venta["Ganancia"],
+                venta["ImporteTotal"],
+                venta["GananciaTotal"],
+                venta["CostoTotal"],
+                venta["Fecha"],
+                venta["Tienda"],
+                venta["CodigoBarra"]
+            ))
+
+            if cursor.rowcount == 0:
+                insert_query = """
+                    INSERT INTO [IMClientesIV].[dbo].[Ventas] (
+                        CodigoCliente,
+                        Cliente,
+                        CodigoTienda,
+                        Tienda,
+                        FechaCreacion,
+                        FechaModificacion,
+                        Fecha,
+                        Año,
+                        Semestre,
+                        Trimestre,
+                        NoMes,
+                        Mes,
+                        Semana,
+                        DiaSemana,
+                        Dia,
+                        CodigoBarra,
+                        CodigoArticulo,
+                        Descripcion,
+                        CodigoColor,
+                        Color,
+                        Talla,
+                        Linea,
+                        Sublinea,
+                        Categoria,
+                        Base,
+                        Genero,
+                        Clasificacion,
+                        LoteOrigen,
+                        Costo,
+                        Precio,
+                        Cantidad,
+                        Ganancia,
+                        ImporteTotal,
+                        GananciaTotal,
+                        CostoTotal
+                    )
+                    VALUES (
+                        ?, ?, NULL, ?,
+                        GETDATE(), GETDATE(),
+                        ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                        ?, ?, ?, ?, ?, ?,
+                        ?, ?, ?, ?, ?, ?, ?,
+                        ?, ?, ?, ?, ?, ?,?
+                    );
+                """
+
+                cursor.execute(insert_query, (
+                    venta["CodigoCliente"],
+                    venta["Cliente"],
+                    #venta["CodigoTienda"],
+                    venta["Tienda"],
+                    venta["Fecha"],
+                    venta["Año"],
+                    venta["Semestre"],
+                    venta["Trimestre"],
+                    venta["NoMes"],
+                    venta["Mes"],
+                    venta["Semana"],
+                    venta["DiaSemana"],
+                    venta["Dia"],
+                    venta["CodigoBarra"],
+                    venta["CodigoArticulo"],
+                    venta["Descripcion"],
+                    venta["CodigoColor"],
+                    venta["Color"],
+                    venta["Talla"],
+                    venta["Linea"],
+                    venta["Sublinea"],
+                    venta["Categoria"],
+                    venta["Base"],
+                    venta["Genero"],
+                    venta["Clasificacion"],
+                    venta["LoteOrigen"],
+                    venta["Costo"],
+                    venta["Precio"],
+                    venta["Cantidad"],
+                    venta["Ganancia"],
+                    venta["ImporteTotal"],
+                    venta["GananciaTotal"],
+                    venta["CostoTotal"]
+                ))
+
         conn.commit()
 
 
@@ -50,7 +173,7 @@ def ventas():
 
     #obtener las facturas de venta
     models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
-    fecha_inicio = datetime.now().date()- pd.Timedelta(days=2)
+    fecha_inicio = datetime.now().date()- pd.Timedelta(days=1)
     fecha_inicio = fecha_inicio.strftime('%Y-%m-%d')
     invoice = pd.DataFrame(models.execute_kw(db, uid, password, 'pos.order', 'search_read',[[['date_order','>',fecha_inicio]]]))
     print(f"Facturas encontradas desde {fecha_inicio}: {len(invoice)}")
@@ -69,76 +192,124 @@ def ventas():
     Clientes = df[df['CodigoCliente'] == "IMGT-000001134"]
 
     #iterar sobre las facturas y obtener los productos vendidos para cada factura
+    registros = []
+    cantidad_lineas = 0
+
     for index, row in invoice.iterrows():
         order_id = row['id']
-        products = pd.DataFrame(models.execute_kw(db, uid, password, 'pos.order.line', 'search_read',[[['order_id','=',order_id]]]))
+        products = pd.DataFrame(models.execute_kw(
+            db, uid, password,
+            'pos.order.line',
+            'search_read',
+            [[['order_id', '=', order_id]]]
+        ))
+
         tienda = row['config_id'][1] if row['config_id'] else 'Desconocida'
 
         print(f"Procesando factura: {row['pos_reference']}: {len(products)} productos")
+
         for index, product in products.iterrows():
-            product_odoo = pd.DataFrame(models.execute_kw(db, uid, password, 'product.product', 'search_read',[[['id','=',product['product_id'][0]]]],{'limit': 1})).rename(columns={'barcode': 'CodigoBarra'})
-            codigo_barra = product_odoo['CodigoBarra'].values[0] if 'CodigoBarra' in product_odoo.columns and len(product_odoo['CodigoBarra']) > 0 else None
+            product_odoo = pd.DataFrame(models.execute_kw(
+                db, uid, password,
+                'product.product',
+                'search_read',
+                [[['id', '=', product['product_id'][0]]]],
+                {'limit': 1}
+            )).rename(columns={'barcode': 'CodigoBarra'})
+
+            codigo_barra = (
+                product_odoo['CodigoBarra'].values[0]
+                if 'CodigoBarra' in product_odoo.columns and len(product_odoo['CodigoBarra']) > 0
+                else None
+            )
+
             if pd.isna(codigo_barra) or codigo_barra in (False, '', None):
-                continue 
-                    
+                continue
+
             try:
-                results = obtener_CodigoDeBarraInfo(codigo_barra) #pd.DataFrame.from_records(rows, columns=columns)
+                results = obtener_CodigoDeBarraInfo(codigo_barra)
             except Exception as e:
                 print(f"Error al obtener información para el código de barra {codigo_barra}: {e}")
                 continue
-            
+
             if results.empty:
                 print(f"No se encontró información para el código de barra {codigo_barra}")
                 continue
-            #fecha venta en formato date
 
-            fecha_venta_dt = pd.to_datetime(row['date_order']).date() if 'date_order' in row and pd.notna(row['date_order']) else None
+            fecha_venta_dt = (
+                pd.to_datetime(row['date_order']).date()
+                if 'date_order' in row and pd.notna(row['date_order'])
+                else None
+            )
+
             fecha_venta = fecha_venta_dt.strftime('%Y-%m-%d') if fecha_venta_dt else None
 
             json_data = {
                 "CodigoCliente": Clientes['CodigoCliente'].values[0],
                 "Cliente": Clientes['Cliente'].values[0],
-                "CodigoTienda": None,
+                #"CodigoTienda": "",
                 "Tienda": tienda,
                 "Fecha": fecha_venta,
                 "Año": fecha_venta_dt.year if fecha_venta_dt else None,
-                #agregar una S para que se mire como S1, S2
                 "Semestre": "S1" if fecha_venta_dt and fecha_venta_dt.month <= 6 else "S2" if fecha_venta_dt else None,
-                #agregar una Q para que se mire como Q1, Q2, Q3, Q4
-                "Trimestre": "Q1" if fecha_venta_dt and fecha_venta_dt.month <= 3 else "Q2" if fecha_venta_dt and fecha_venta_dt.month <= 6 else "Q3" if fecha_venta_dt and fecha_venta_dt.month <= 9 else "Q4" if fecha_venta_dt else None,
+                "Trimestre": (
+                    "Q1" if fecha_venta_dt and fecha_venta_dt.month <= 3 else
+                    "Q2" if fecha_venta_dt and fecha_venta_dt.month <= 6 else
+                    "Q3" if fecha_venta_dt and fecha_venta_dt.month <= 9 else
+                    "Q4" if fecha_venta_dt else None
+                ),
                 "NoMes": fecha_venta_dt.month if fecha_venta_dt else None,
                 "Mes": fecha_venta_dt.strftime('%B') if fecha_venta_dt else None,
                 "Semana": fecha_venta_dt.isocalendar()[1] if fecha_venta_dt else None,
                 "DiaSemana": fecha_venta_dt.strftime('%A') if fecha_venta_dt else None,
                 "Dia": fecha_venta_dt.day if fecha_venta_dt else None,
                 "CodigoBarra": codigo_barra,
-                "CodigoArticulo": results['CodigoArticulo'].values[0] if 'CodigoArticulo' in results.columns and len(results['CodigoArticulo']) > 0 else None,
-                "Descripcion": results['Descripcion'].values[0] if 'Descripcion' in results.columns and len(results['Descripcion']) > 0 else None,
-                "CodigoColor": results['CodigoColor'].values[0] if 'CodigoColor' in results.columns and len(results['CodigoColor']) > 0 else None,
-                "Color": results['Color'].values[0] if 'Color' in results.columns and len(results['Color']) > 0 else None,
-                "Talla": results['Talla'].values[0] if 'Talla' in results.columns and len(results['Talla']) > 0 else None,
-                "Linea": results['Linea'].values[0] if 'Linea' in results.columns and len(results['Linea']) > 0 else None,
-                "Sublinea": results['Sublinea'].values[0] if 'Sublinea' in results.columns and len(results['Sublinea']) > 0 else None,
-                "Categoria": results['Categoria'].values[0] if 'Categoria' in results.columns and len(results['Categoria']) > 0 else None,
-                "Base": results['Base'].values[0] if 'Base' in results.columns and len(results['Base']) > 0 else None,
-                "Genero": results['Genero'].values[0] if 'Genero' in results.columns and len(results['Genero']) > 0 else None,
-                "Clasificacion": results['Clasificacion'].values[0] if 'Clasificacion' in results.columns and len(results['Clasificacion']) > 0 else None,
-                "LoteOrigen": results['LoteOrigen'].values[0] if 'LoteOrigen' in results.columns and len(results['LoteOrigen']) > 0 else None,
-                "Costo": None,
+                "CodigoArticulo": results['CodigoArticulo'].values[0] if 'CodigoArticulo' in results.columns else None,
+                "Descripcion": results['Descripcion'].values[0] if 'Descripcion' in results.columns else '',
+                "CodigoColor": results['CodigoColor'].values[0] if 'CodigoColor' in results.columns else '',
+                "Color": results['Color'].values[0] if 'Color' in results.columns else '',
+                "Talla": results['Talla'].values[0] if 'Talla' in results.columns else '',
+                "Linea": results['Linea'].values[0] if 'Linea' in results.columns else '',
+                "Sublinea": results['Sublinea'].values[0] if 'Sublinea' in results.columns else '',
+                "Categoria": results['Categoria'].values[0] if 'Categoria' in results.columns else '',
+                "Base": results['Base'].values[0] if 'Base' in results.columns else '',
+                "Genero": results['Genero'].values[0] if 'Genero' in results.columns else '',
+                "Clasificacion": results['Clasificacion'].values[0] if 'Clasificacion' in results.columns else '',
+                "LoteOrigen": results['LoteOrigen'].values[0] if 'LoteOrigen' in results.columns else '',
+                "Costo": 0,
                 "Precio": product['price_unit'],
                 "Cantidad": int(product['qty']),
-                "Ganacia": None,
-                "ImporteTotal": product['price_unit'] * product['qty'] if 'price_unit' in product and 'qty' in product and pd.notna(product['price_unit']) and pd.notna(product['qty']) else None,
-                "GananciaTotal": None,
-                "CostoTotal": None
+                "Ganancia": 0,
+                "ImporteTotal": (
+                    product['price_unit'] * product['qty']
+                    if pd.notna(product['price_unit']) and pd.notna(product['qty'])
+                    else 0
+                ),
+                "GananciaTotal": 0,
+                "CostoTotal": 0
             }
 
-            json_data = json.dumps(json_data, ensure_ascii=False)  # Convertir a JSON string    
-            enviar_IntermodaUnikFashion(json_data)
+            registros.append(json_data)
+            cantidad_lineas += 1
+    df = pd.DataFrame(registros)
+
+    columnas_sumar = ["Cantidad", "ImporteTotal", "GananciaTotal", "CostoTotal"]
+
+    columnas_agrupar = [
+        col for col in df.columns
+        if col not in columnas_sumar
+    ]
+
+    df_agrupado = (
+        df.groupby(columnas_agrupar, dropna=False, as_index=False)[columnas_sumar]
+        .sum()
+    )
+
+    for _, row in df_agrupado.iterrows():
+        json_data = json.dumps(row.to_dict(), ensure_ascii=False)
+        print(json_data)
+        enviar_IntermodaUnikFashion(json_data)
+
     print("Proceso completado")
 
-
-if __name__ == '__main__':
-    ventas()
-        
- 
+            
